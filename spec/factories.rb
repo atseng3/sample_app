d8aafa30ee8fd481506a4bce3f5a8800fe6ee896
Albert Tseng <atseng3@gmail.com>
2013-09-07 14:06:36 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+     factory :user do
+          name "Albert Tseng"
+          email "atseng3@gmail.com"
+          password "881031"
+          password_confirmation "881031"
+     end
+end
